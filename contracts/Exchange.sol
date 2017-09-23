41bc44efafbf34de52a54e21d52a1af93af4ae19
Oleksandr Brezhniev <oleksandr.brezhniev@gmail.com>
2017-09-24 01:04:59 +0300

    Changes to Exchange

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -1,12 +1,6 @@
 pragma solidity ^0.4.0;
-contract TicketRegistry {
-    function getOwner(uint ticketId) public returns (address owner) {
-        return 0xca35b7d915458ef540ade6068dfe2f44e8fa733c;
-    }
-    function setOwner(uint ticketId, address owner) public {
-        //return 0xca35b7d915458ef540ade6068dfe2f44e8fa733c;
-    }
-}
+
+import "TicketRegistry.sol";
 
 contract Exchange {
 
@@ -16,7 +10,7 @@ contract Exchange {
         address refundAddress;
         address transferAddress;
     }
-    
+
     Proposal[] proposals;
     bool proposalsFinilized = false;
 
@@ -26,11 +20,11 @@ contract Exchange {
     }
 
     function setPropasal(uint8 proposalId, address ticketRegistryContract, uint ticketId, address refundAddress, address transferAddress) {
-        require(proposalsFinilized);
-        require(proposalId >= proposals.length);
-        require(ticketRegistryContract == 0);
-        require(refundAddress == 0);
-        require(transferAddress == 0);
+        require(!proposalsFinilized);
+        require(proposalId < proposals.length);
+        require(ticketRegistryContract != 0);
+        require(refundAddress != 0);
+        require(transferAddress != 0);
         proposals[proposalId].ticketRegistryContract = ticketRegistryContract;
         proposals[proposalId].ticketId = ticketId;
         proposals[proposalId].refundAddress = refundAddress;
@@ -39,8 +33,7 @@ contract Exchange {
 
     function finilizeProposals() {
         for (uint8 proposalId = 0; proposalId < proposals.length; proposalId++) {
-            if (proposals[proposalId].ticketRegistryContract == 0) 
-            throw;
+            require(proposals[proposalId].ticketRegistryContract != 0);
         }
         proposalsFinilized = true;
     }
@@ -48,28 +41,19 @@ contract Exchange {
     function refund() {
         for (uint8 proposalId = 0; proposalId < proposals.length; proposalId++) {
             TicketRegistry tr = TicketRegistry(proposals[proposalId].ticketRegistryContract);
-	    	if (tr.getOwner(proposals[proposalId].ticketId) == address(this)) {
-	    	    tr.setOwner(proposals[proposalId].ticketId, proposals[proposalId].refundAddress);
+	    	if (tr.ticketMap(proposals[proposalId].ticketId) == address(this)) {
+	    	    tr.transfer(proposals[proposalId].ticketId, proposals[proposalId].refundAddress);
 	    	}
 		}
-		//kill();
+		selfdestruct();
     }
 
     function transfer() {
         for (uint8 proposalId = 0; proposalId < proposals.length; proposalId++) {
-            checkProposal(proposalId);
             TicketRegistry tr = TicketRegistry(proposals[proposalId].ticketRegistryContract);
-	    	if (tr.getOwner(proposals[proposalId].ticketId) == address(this)) {
-	    	    tr.setOwner(proposals[proposalId].ticketId, proposals[proposalId].transferAddress);
-	    	}
+	    	require(tr.ticketMap(proposals[proposalId].ticketId) == address(this));
+	    	require(tr.transfer(proposals[proposalId].ticketId, proposals[proposalId].transferAddress));
 		}
-		//kill();
-    }
-
-    function checkProposal(uint8 proposalId) returns (bool successful) {
-        if (proposalId >= proposals.length) 
-        throw;
-        TicketRegistry tr = TicketRegistry(proposals[proposalId].ticketRegistryContract);
-		return tr.getOwner(proposals[proposalId].ticketId) == address(this);
+		selfdestruct();
     }
 }
