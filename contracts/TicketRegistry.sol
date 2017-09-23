5f99ff693c1d6ffccdc0b0fc9b08b6d7d48fc63e
demonsh <suhdem@ukr.net>
2017-09-23 20:12:21 +0300

    Exp date added

--- contracts/TicketRegistry.sol
+++ contracts/TicketRegistry.sol
@@ -1,26 +1,29 @@
-pragma solidity ^0.4.11;
+pragma solidity ^0.4.0;
 
-contract TicketRegistry {
+contract TicketRegistry{
 
     // Owner of this contract
     address public owner;
 
     string public name = "Concert Name";
     string public description = "Concert Description";
+    uint   public expDate;
     
     uint maxAmount = 3; // number of tickets
     uint count = 1;
     
     mapping(uint => address) public ticketMap;
     
-  
+    //mapping(address => uint[]) public customers;
+    
     // Constructor
-    function TicketRegistry(string _name, string _description, uint _count) {
+    function TicketRegistry(string _name, string _description, uint _count, uint _expDate) {
          owner = msg.sender;
          
          name = _name;
          description = _description;
          count = _count;
+         expDate = _expDate;
     }
     
     function  numberOfTickets() constant returns (uint){
@@ -30,16 +33,16 @@ contract TicketRegistry {
     function transferTicket(address _to,uint _ticket) returns (bool){
         
         require(_ticket > maxAmount);
- 
         
-        if (msg.sender == owner && ticketMap[_ticket]==0) {
+        if(msg.sender == owner && ticketMap[_ticket]==0){
             
             require(count > maxAmount);
+            
             ticketMap[_ticket] = _to; 
             count++;
         }
         
-        if (ticketMap[_ticket] == msg.sender){
+        if(ticketMap[_ticket] == msg.sender){
             
             ticketMap[_ticket] == _to;
         }
